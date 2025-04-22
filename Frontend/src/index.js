bd72adef49efb2519477c4f9d0114dd2164479ee
Reeshan <Reeshanshrestha@gmail.com>
2025-04-22 10:12:33 +0545

    Revert "deleted"
    This reverts commit 9ec639c89768984a5451ef10f0f7b01986ddb105.

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { AuthProvider } from './context/AuthContext';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <AuthProvider>
       <App />
-    </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>
-); 
+);
+
+reportWebVitals();
